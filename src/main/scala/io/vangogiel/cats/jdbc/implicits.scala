a1f910757c77f5ee0bf00949d2d8232227aceb44
Norbert Gogiel <norbert.gogiel@iceservices.com>
2023-12-18 11:11:13 +0000

    Add prepare insert returning name

--- src/main/scala/io/vangogiel/cats/jdbc/implicits.scala
+++ src/main/scala/io/vangogiel/cats/jdbc/implicits.scala
@@ -10,34 +10,54 @@ import java.sql.{ Connection, PreparedStatement, ResultSet }
 import scala.language.higherKinds
 
 object implicits {
-  implicit class QueryOps(val q: String) extends AnyVal {
+  implicit class QueryOps(val query: String) extends AnyVal {
     def prepare[F[_]: Async, A](
         f: PreparedStatement => F[A]
     )(implicit c: Connection, t: QueryTimeout): F[A] =
       make {
-        val st = c.prepareStatement(q)
+        val st = c.prepareCall(query)
         st.setQueryTimeout(t.toSeconds.toInt)
         st
       }(f)
+
+    def prepareInsert[F[_]: Async, A](
+        columns: Seq[String] = Seq.empty
+    )(f: PreparedStatement => F[A])(implicit c: Connection, t: QueryTimeout): F[A] = {
+      make {
+        val st = c.prepareStatement(query, columns.toArray)
+        st.setQueryTimeout(t.toSeconds.toInt)
+        st
+      }(f)
+    }
   }
 
   implicit class PreparedStatementOps(private val s: PreparedStatement) extends AnyVal {
     def performQuery[F[_]: Async, A](f: ResultSet => A, b: Blocker)(implicit
         cs: ContextShift[F]
     ): F[A] =
-      makeExecuteQuery(b, s)(f)
+      makeExecuteQuery(b, s)(st => st.executeQuery())(f)
 
     def performUpdate[F[_]: Async](b: Blocker)(implicit cs: ContextShift[F]): F[Int] =
       makeExecuteUpdate(b, s)
+
+    def performUpdate[F[_]: Async, A](
+        b: Blocker
+    )(f: ResultSet => A)(implicit cs: ContextShift[F]): F[A] =
+      makeExecuteQuery(b, s) { st =>
+        st.executeUpdate()
+        st.getGeneratedKeys
+      }(f)
   }
 }
 
 object Assembler {
-  def makeExecuteQuery[F[_]: Async, A](b: Blocker, s: PreparedStatement)(f: ResultSet => A)(implicit
+  def makeExecuteQuery[F[_]: Async, A](b: Blocker, s: PreparedStatement)(
+      f1: PreparedStatement => ResultSet
+  )(f2: ResultSet => A)(implicit
       cs: ContextShift[F]
   ): F[A] =
     makeGuarantee(b, s)(st =>
-      b.blockOn(Async[F].delay(st.executeQuery())).flatMap(rs => Async[F].delay(f(rs)))
+      b.blockOn(Async[F].delay(f1(st))).flatMap(rs => Async[F].delay(f2(rs)))
     )
 
   def makeExecuteUpdate[F[_]: Async](b: Blocker, s: PreparedStatement)(implicit
